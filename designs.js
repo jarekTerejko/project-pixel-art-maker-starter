3c0d236771cad513578be0211299709ddc6cdddb
Jarosław Terejko <j.terejko@gmail.com>
2018-01-26 22:06:22 +0100

    first commit

--- designs.js
+++ designs.js
@@ -3,8 +3,30 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+//function makeGrid() {
+//
+//    // Your code goes here!
+//
+//}
 
-// Your code goes here!
+$(document).ready(function () {
+    $('#sizePicker').submit(function makeGrid(noSubmit) {
+        $('tr').remove();
+        var row = $('#input_height').val();
+        var col = $('#input_width').val();
+        for (var i = 1; i <= row; i++) {
+            $('#pixel_canvas').append('<tr></tr>');
+            for (var j = 1; j <= col; j++) {
+                $('tr:last').append('<td></td>');
+                $('td').attr("class", 'color-me');
+            }
+        }
+        noSubmit.preventDefault();
+        
+        $('.color-me').click(function(event) {
+            var paint = $('#color_picker').val();
+            $(event.target).css('background-color', paint);
+        });
+    });
+});
 
-}
